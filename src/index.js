94bd7c735e48d16ffee3581c9ba06a16bf64ee04
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2023-01-26 15:16:16 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import 'modern-normalize/modern-normalize.css';
 import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="react-team-project">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
