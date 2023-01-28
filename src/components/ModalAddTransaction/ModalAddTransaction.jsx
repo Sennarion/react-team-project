d07754ced97c538aaf34abce89b3f449a202b00f
annaKumchar <anna.kumchar@gmail.com>
2023-01-28 12:30:27 +0200

    Modal Markup

--- src/components/ModalAddTransaction/ModalAddTransaction.jsx
+++ src/components/ModalAddTransaction/ModalAddTransaction.jsx
@@ -1,3 +1,44 @@
+import {
+  Backdrop,
+  Modal,
+  CloseButton,
+  ModalTitle,
+  SwitchWrap,
+  Switch,
+  TransactionForm,
+  SumInput,
+} from './ModalAddTransaction.styled';
+import { Formik } from 'formik';
+
+const initialValues = {
+  sum: '',
+  category: '',
+  comment: '',
+  date: new Date(),
+};
+
 export default function ModalAddTransaction() {
-  return <div>ModalAddTransaction</div>;
+  return (
+    <Backdrop>
+      <Modal>
+        <CloseButton />
+        <ModalTitle>Add transaction</ModalTitle>
+        <SwitchWrap>
+          <Switch></Switch>
+        </SwitchWrap>
+        <Formik initialValues={initialValues}>
+          <TransactionForm>
+            <SumInput
+              name="sum"
+              type="number"
+              min="0.01"
+              step="0.01"
+              // value={sum}
+              placeholder="0.00"
+            />
+          </TransactionForm>
+        </Formik>
+      </Modal>
+    </Backdrop>
+  );
 }
---
+++ src/components/ModalAddTransaction/ModalAddTransaction.styled.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import { Form, Field } from 'formik';
+
+export const Backdrop = styled.div`
+`;
+
+export const Modal = styled.div`
+`;
+
+export const CloseButton = styled.button`
+`;
+
+export const ModalTitle = styled.h2`
+`;
+
+export const SwitchWrap = styled.div`
+`;
+
+export const Switch = styled.button`
+`;
+
+export const TransactionForm = styled(Form)`
+`;
+
+export const SumInput = styled(Field)`
+`;
+
+
+
