703217f9f3aa77138e091f4baefc7b0e5c3878fe
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2023-01-29 14:26:29 +0200

    add logout modal

--- src/components/ModalLogout/ModalLogout.jsx
+++ src/components/ModalLogout/ModalLogout.jsx
@@ -1,3 +1,17 @@
+import Backdrop from 'components/UI/Backdrop/Backdrop';
+import { ModalWrapper, ModalTitle, ButtonsWrapper } from './ModalLogout.styled';
+import Button from 'components/UI/Button/Button';
+
 export default function ModalLogout() {
-  return <div>ModalLogout</div>;
+  return (
+    <Backdrop>
+      <ModalWrapper>
+        <ModalTitle>Are you sure you want to exit?</ModalTitle>
+        <ButtonsWrapper>
+          <Button>Yes</Button>
+          <Button>No</Button>
+        </ButtonsWrapper>
+      </ModalWrapper>
+    </Backdrop>
+  );
 }
