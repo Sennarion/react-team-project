eee16f17b40e203b9ab49fa3488b3cf62fe3c457
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2023-01-29 11:39:50 +0200

    add balance status

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -2,6 +2,7 @@ export const getIsLoggedIn = state => state.auth.isLogged;
 export const getUsername = state => state.auth.user.username;
 export const getIsRefreshCurrentUser = state => state.auth.isRefreshUser;
 export const getToken = state => state.auth.token;
+export const getUserBalance = state => state.auth.user.balance;
 
 const authSelectors = {
   getIsLoggedIn,
