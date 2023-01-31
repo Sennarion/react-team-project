70c61655cbc40eae39b47c38a553373178e89bb3
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2023-01-31 19:53:25 +0200

    add formatCurrency func

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,21 +1,14 @@
 import { useSelector } from 'react-redux';
 import { getBalance } from 'redux/auth/selectors';
 import { BalanceWrapper, Desc, BalanceStatus } from './Balance.styled';
+import { formatCurrency } from 'utils/formatCurrency';
 
 export default function Balance() {
   const balance = useSelector(getBalance);
   return (
     <BalanceWrapper>
       <Desc>Your balance</Desc>
-      <BalanceStatus>
-        {balance
-          .toLocaleString('en-US', {
-            style: 'currency',
-            currency: 'USD',
-          })
-          .replace('$', '₴ ')
-          .replace(',', ' ')}
-      </BalanceStatus>
+      <BalanceStatus>₴ {formatCurrency(balance)}</BalanceStatus>
     </BalanceWrapper>
   );
 }
