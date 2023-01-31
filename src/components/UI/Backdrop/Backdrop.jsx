6f290382327dfc03816a1e0aec76d3a5b8cac7f3
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2023-01-31 12:39:55 +0200

    add modal open animation

--- src/components/UI/Backdrop/Backdrop.jsx
+++ src/components/UI/Backdrop/Backdrop.jsx
@@ -1,9 +1,5 @@
-import { createPortal } from 'react-dom';
 import { BackdropWrapper } from './Backdrop.styled';
 
 export default function Backdrop({ children, ...props }) {
-  return createPortal(
-    <BackdropWrapper {...props}>{children}</BackdropWrapper>,
-    document.querySelector('#popup-root')
-  );
+  return <BackdropWrapper {...props}>{children}</BackdropWrapper>;
 }
